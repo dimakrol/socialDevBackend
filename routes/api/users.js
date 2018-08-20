8103007c29568042d372494c4b429dd803920a6b
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2018-08-20 17:57:11 +0300

    Added endpoint for user registration.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+//Load User model
+const User = require('../../models/Users');
 
 /**
  * @route GET api/users/test
@@ -7,5 +12,42 @@ const router = express.Router();
  * @access Public
  */
 router.get('/test', (req, res) => res.json({a: 'test users'}));
+
+/**
+ * @route GET api/users/register
+ * @desc Register user
+ * @access Public
+ */
+router.post('/register', ({body}, res) => {
+    User.findOne({ email: body.email})
+        .then(user => {
+            if (user) {
+                return res.status(400).json({email: 'Email already exists'})
+            } else {
+                const avatar = gravatar.url(body.email, {
+                    s: '200', //size
+                    r: 'pg', //rating,
+                    d: 'mm', //Default
+                });
+
+                const newUser = new User({
+                    name: body.name,
+                    email: body.email,
+                    avatar,
+                    password: body.password
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err))
+                    })
+                })
+            }
+        })
+});
 
 module.exports = router;
