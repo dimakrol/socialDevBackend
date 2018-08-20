10ae32a3d5a46ab1ad618f85a1b0923db5cf3740
dimakrol <dmitriy.krol.v@gmail.com>
2018-08-21 00:32:47 +0300

    Refactoring.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const users = require('./api/users');
+
 const profile = require('./api/profile');
+
 const posts = require('./api/posts');
+
 
 module.exports = (app) => {
     //api routes
+    //here we can set additional middleware
     app.use('/api/users', users);
     app.use('/api/profile', profile);
     app.use('/api/posts', posts);
