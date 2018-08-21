2b6d84301890849016728b4260df4ad549afdf9d
dimakrol <dmitriy.krol.v@gmail.com>
2018-08-22 00:27:18 +0300

    Added ability to create posts.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Post model
+const Post = require('../../models/Post');
+//Validation
+const validatePostInput = require('../../validation/post');
 
 /**
  * @route GET api/posts/test
@@ -7,5 +14,33 @@ const router = express.Router();
  * @access Public
  */
 router.get('/test', (req, res) => res.json({a: 'test posts'}));
+
+/**
+ * @route Post api/posts
+ * @desc Create post
+ * @access Private
+ */
+router.post('/',
+    passport.authenticate('jwt', { session: false }),
+    ({body, user}, res) => {
+
+        const {errors, isValid} = validatePostInput(body);
+
+        //Check Validation
+        if (!isValid) {
+            //Return errors
+            return res.status(400).json(errors);
+        }
+
+        const newPost = new Post({
+            text: body.text,
+            name: body.name,
+            avatar: body.avatar,
+            user: user.id
+        });
+
+        newPost.save()
+            .then(post => res.json(post))
+        });
 
 module.exports = router;
