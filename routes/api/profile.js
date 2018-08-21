86f57cf8c29e2fb60fe3b02202f7050cb8ee4b98
dimakrol <dmitriy.krol.v@gmail.com>
2018-08-21 22:27:01 +0300

    Added routes additional routes for profiles.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -38,6 +38,64 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 });
 
 /**
+ * @route GET api/profile/all
+ * @desc Get all profiles
+ * @access Public
+ */
+router.get('/all', (req, res) => {
+    const errors = {};
+    Profile.find()
+        .populate('user', ['name', 'avatar'])
+        .then(profiles => {
+            if(!profiles) {
+                errors.noprofiles = 'There are no profiles';
+                return res.status(404).json(errors)
+            }
+            res.json(profiles);
+        })
+        .catch(err => res.status(404).json({noprofiles: 'There are no profiles'}))
+
+});
+
+/**
+ * @route GET api/profile/handle/:handle
+ * @desc Get profile by handle
+ * @access Public
+ */
+router.get('/handle/:handle', ({params}, res) => {
+    const errors = {};
+    Profile.findOne({ handle: params.handle })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if(!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.status(404).json(errors);
+            }
+            res.json(profile)
+        })
+        .catch(err => res.status(404).json(err))
+});
+
+/**
+ * @route GET api/profile/user/:user_id
+ * @desc Get profile by user id
+ * @access Public
+ */
+router.get('/user/:user_id', ({params}, res) => {
+    const errors = {};
+    Profile.findOne({ user: params.user_id })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if(!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.status(404).json(errors);
+            }
+            res.json(profile)
+        })
+        .catch(err => res.status(404).json({profile: 'There is no profile for this user'}))
+});
+
+/**
  * @route Post api/profile
  * @desc Create or Edit user profile
  * @access Private
