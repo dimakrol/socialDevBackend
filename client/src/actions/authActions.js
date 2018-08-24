9f43f8548bb95f54ad61bfaac518fe1a07be641b
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2018-08-24 19:43:56 +0300

    Prepare for user login functionality.

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
 // Register User
 export const registerUser = (userData, history) => dispatch => {
     axios.post('/api/users/register', userData)
@@ -10,10 +13,35 @@ export const registerUser = (userData, history) => dispatch => {
                     payload: err.response.data
                 })
             })
-
-    // return {
-    //     type: TEST_DISPATH,
-    //     payload: userData
-    // }
 };
 
+// Login - get user token
+export const loginUser = (userData) => dispatch => {
+    axios.post('api/users/login', userData)
+        .then(res => {
+            // Save to localStorage
+            const { token } = res.data;
+            // Set token to ls
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth header
+            setAuthToken(token);
+            // Decode token to get user data
+            const decode = jwt_decode(token);
+            // Set current user
+            dispatch(setCurrentUser(decode));
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+};
+
+// Set logged in user
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
