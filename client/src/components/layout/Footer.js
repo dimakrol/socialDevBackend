ee5dbf9b1f18c798059989320bf881d6bb88c393
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2018-08-29 18:38:12 +0300

    Added add experience page.

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default () => {
     return (
         <div>
-            <footer className="bg-dark text-white mt-5 p-4">
-                Copyright &copy: {new Date().getFullYear()} Dev Social
+            <footer className="bg-dark text-white mt-5 p-4 text-center">
+                Copyright &copy; {new Date().getFullYear()} Dev Social
             </footer>
         </div>
     );
